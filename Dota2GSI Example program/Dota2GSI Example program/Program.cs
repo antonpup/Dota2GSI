ecbbe07f6c6c89ce3b8c44caa55fc98b1a85f46d
Anton Pupkov <pupant@gmail.com>
2024-01-11 12:08:26 -0800

    Update example program

--- Dota2GSI Example program/Dota2GSI Example program/Program.cs
+++ Dota2GSI Example program/Dota2GSI Example program/Program.cs
@@ -15,8 +15,6 @@ namespace Dota2GSI_Example_program
 
         static void Main(string[] args)
         {
-            if (args == null) Console.WriteLine();
-
             CreateGsifile();
 
             Process[] pname = Process.GetProcessesByName("Dota2");
@@ -29,7 +27,6 @@ namespace Dota2GSI_Example_program
 
             _gsl = new GameStateListener(4000);
             _gsl.NewGameState += OnNewGameState;
-
 
             if (!_gsl.Start())
             {
@@ -87,8 +84,21 @@ namespace Dota2GSI_Example_program
             else
             {
                 Console.WriteLine("Your steam name: " + gs.Player.Name);
-                Console.WriteLine("hero ID: " + gs.Hero.ID);
-                Console.WriteLine("Health: " + gs.Hero.Health);
+                Console.WriteLine("Your dota account id: " + gs.Player.AccountID);
+
+                Console.WriteLine("Your hero healing: " + gs.Player.HeroHealing);
+                Console.WriteLine("Your hero damage: " + gs.Player.HeroDamage);
+                Console.WriteLine("Your hero tower damage: " + gs.Player.TowerDamage);
+
+                Console.WriteLine("Radiant Score: " + gs.Map.RadiantScore);
+                Console.WriteLine("Dire Score: " + gs.Map.DireScore);
+                Console.WriteLine("Is game paused: " + gs.Map.IsPaused);
+
+                Console.WriteLine("Hero ID: " + gs.Hero.ID);
+                Console.WriteLine("Hero XP: " + gs.Hero.Experience);
+                Console.WriteLine("Hero has Aghanims Shard upgrade: " + gs.Hero.HasAghanimsShardUpgrade);
+
+                Console.WriteLine("Hero Health: " + gs.Hero.Health);
                 for (int i = 0; i < gs.Abilities.Count; i++)
                 {
                     Console.WriteLine("Ability {0} = {1}", i, gs.Abilities[i].Name);
@@ -100,10 +110,17 @@ namespace Dota2GSI_Example_program
                 Console.WriteLine("Fifth slot inventory: " + gs.Items.GetInventoryAt(4).Name);
                 Console.WriteLine("Sixth slot inventory: " + gs.Items.GetInventoryAt(5).Name);
 
+                Console.WriteLine("Teleport item slot: " + gs.Items.Teleport.Name);
+                Console.WriteLine("Neutral item slot: " + gs.Items.Neutral.Name);
+
                 if (gs.Items.InventoryContains("item_blink"))
+                {
                     Console.WriteLine("You have a blink dagger");
+                }
                 else
+                {
                     Console.WriteLine("You DO NOT have a blink dagger");
+                }
 
                 Console.WriteLine("Talent Tree:");
                 for (int i = gs.Hero.TalentTree.Length - 1; i >= 0; i--)
@@ -138,12 +155,21 @@ namespace Dota2GSI_Example_program
                     "    \"heartbeat\"     \"30.0\"",
                     "    \"data\"",
                     "    {",
-                    "        \"provider\"      \"1\"",
-                    "        \"map\"           \"1\"",
-                    "        \"player\"        \"1\"",
-                    "        \"hero\"          \"1\"",
-                    "        \"abilities\"     \"1\"",
-                    "        \"items\"         \"1\"",
+                    "        \"provider\"       \"1\"",
+                    "        \"map\"            \"1\"",
+                    "        \"player\"         \"1\"",
+                    "        \"hero\"           \"1\"",
+                    "        \"abilities\"      \"1\"",
+                    "        \"items\"          \"1\"",
+                    "        \"events\"         \"1\"",
+                    "        \"buildings\"      \"1\"",
+                    "        \"league\"         \"1\"",
+                    "        \"draft\"          \"1\"",
+                    "        \"wearables\"      \"1\"",
+                    "        \"minimap\"        \"1\"",
+                    "        \"roshan\"         \"1\"",
+                    "        \"couriers\"       \"1\"",
+                    "        \"neutralitems\"   \"1\"",
                     "    }",
                     "}",
 
